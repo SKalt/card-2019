762ba8eadc613d6951acb99de63a2362809880ee
Steven Kalt <kalt.steven@gmail.com>
2019-11-28 09:42:58 -0500

    fixed an idiotic transparency issue

--- src/main.ts
+++ src/main.ts
@@ -7,52 +7,67 @@ import {
 } from "./lib";
 import snow from "./img/snow.png";
 
+export function imagedata_to_image(imagedata) {
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  canvas.width = imagedata.width;
+  canvas.height = imagedata.height;
+  ctx.putImageData(imagedata, 0, 0);
+
+  var image = new Image();
+  image.src = canvas.toDataURL();
+  return image;
+}
 async function main() {
   const canvas: HTMLCanvasElement = document.querySelector(
     "canvas.holiday-card--picture_animation"
   );
 
-  // imageOf(snow).then(img => {
-  //   document.querySelectorAll(".holiday-card--picture_animation").forEach(e => {
-  //     e.style.height = img.height;
-  //     setTimeout(() => {
-  //       alert("transform");
-  //       e.style.transform = `translate(10px, 10px)`;
-  //     }, 5000);
-  //   });
+  imageOf(snow).then(img => {
+    console.log(`main--snow, ${img.width}, h ${img.height}`);
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const imgData = getImageData(img);
+    const bitArray = asBitArray(imgData);
+    const otherImgData = colorBitArrayAsImageData([255, 255, 255], bitArray);
+    const ctx = canvas.getContext("2d");
+    const layers = [
+      // layer({}) // rearmost must come first
+      // layer({})(ctx)
+      layer({ matrix: [0.3, 0.3, 0.7], bitArray, color: [255, 255, 255] })
+    ];
 
-  // canvas.width = img.width;
-  // canvas.height = img.height;
-  // const ctx = canvas.getContext("2d");
-  // const imgData = getImageData(img);
-  // const bitArray = asBitArray(getImageData(img));
-  // const otherImgData = colorBitArrayAsImageData([255, 255, 255], bitArray);
-  // console.log({ otherImgData, bitArray });
-  // ctx.putImageData(otherImgData, 0, 0);
-  // layer({ ctx, matrix: [0.3, 0.3, 0.7], bitArray, color: [255, 255, 255] })();
-  // });
+    setInterval(() => {
+      ctx.fillStyle = "#0e121d";
+      ctx.fillRect(0, 0, img.width, img.height);
+      layers.forEach((draw, index) => {
+        console.log(index, draw(ctx));
+      });
+    }, 300);
+  });
 }
+main();
 
 // const worker = new Worker("worker.js");
 // worker.onmessage = function(event) {
 //   console.log(event.data);
 // };
 // main();
-function main2() {
-  const foreground = document.querySelector(
-    ".holiday-card--picture_foreground"
-  );
-  const backgrounds = [
-    ...document.querySelectorAll(".holiday-card--picture_animation")
-  ];
-  foreground.onload = () => {
-    let height = window.getComputedStyle(foreground).height;
-    backgrounds.forEach(e => (e.style.height = height));
-  };
-  document
-    .querySelectorAll(".holiday-card--picture_animation")
-    .forEach(e => {});
-  //     e.style.height = img.height;
-}
+// function main2() {
+//   const foreground = document.querySelector(
+//     ".holiday-card--picture_foreground"
+//   );
+//   const backgrounds = [
+//     ...document.querySelectorAll(".holiday-card--picture_animation")
+//   ];
+//   foreground.onload = () => {
+//     let height = window.getComputedStyle(foreground).height;
+//     backgrounds.forEach(e => (e.style.height = height));
+//   };
+//   document
+//     .querySelectorAll(".holiday-card--picture_animation")
+//     .forEach(e => {});
+//   //     e.style.height = img.height;
+// }
 
-main2();
+// main2();
